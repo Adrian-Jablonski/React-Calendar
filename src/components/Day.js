23207399dfa88151ed41340ebccf7791cad08b62
Adrian-Jablonski <Jablonski.Adrian1@gmail.com>
2019-06-29 17:12:52 -0500

    Added hover event for date that shows add calendar event icon

--- src/components/Day.js
+++ src/components/Day.js
@@ -1,18 +1,58 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCalendarPlus } from "@fortawesome/free-solid-svg-icons";
 
-const Day = ({dayNumb, isCurrentMonth, isCuurrentDate}) => {
-    let dayClass = 'day';
+class Day extends Component {
 
-    if (isCuurrentDate) {
-        dayClass = 'day current-date'
-    }
-    else if (!isCurrentMonth) {
-        dayClass = 'day current-month'
+    state = {
+        visible: false,
     }
 
-    return (
-        <td className={dayClass}>{dayNumb}</td>
-    )
+    displayPlusButton = () => {
+        this.setState(() => {
+            return {
+                visible: true
+            }
+        })
+    }
+
+    hidePlusButton = () => {
+        this.setState(() => {
+            return {
+                visible: false
+            }
+        })
+    }
+
+    render() {
+        const {dayNumb, isCurrentMonth, isCuurrentDate} = this.props
+        let dayClass = 'day';
+
+        if (isCuurrentDate) {
+            dayClass = 'day current-date'
+        }
+        else if (!isCurrentMonth) {
+            dayClass = 'day current-month'
+        }
+
+        let btnClassName = 'plus-button';
+        if (!this.state.visible) {
+            btnClassName = 'plus-button hide';
+        }
+
+        // Add onMouseEnter
+        return (
+            <td className={dayClass}
+                onMouseEnter={() => this.displayPlusButton()} 
+                onMouseLeave={() => this.hidePlusButton()}
+            >
+                <span>{dayNumb}</span>
+                <button className={btnClassName}>
+                    <FontAwesomeIcon icon={faCalendarPlus} />
+                </button>
+            </td>
+        )
+    }
 }
 
 export default Day;
